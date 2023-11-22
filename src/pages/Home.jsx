bef385b12e175f6e794cc1e2a17b0391b94190a1
Leki <lekisintole82@gmail.com>
2023-11-23 00:09:33 +0300

    portfolio

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import About from './About';
+import Services from './Services';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+
+const Home = () => {
+  return (
+    <div>
+      <About />
+      <Services />
+      <Portfolio />
+      <Contact />
+    </div>
+  );
+};
+
+export default Home;
+
+
