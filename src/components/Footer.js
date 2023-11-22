17b7c9ae67bf5f61bf0b081f59847ff4eaab8e94
Leki <lekisintole82@gmail.com>
2023-11-22 23:12:14 +0300

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+         <div className="copyright">
+          <p>
+             © 2023 Leki Emmanuel. <i className="fab fa-heart"></i> All Rights
+             Reserved.
+          </p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
