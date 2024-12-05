afcfb8817b926262b71e3467d25639db76d4979d
Leki <lekisintole82@gmail.com>
2024-12-05 21:08:11 +0300

    period

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div>
-         <div className="copyright">
-          <p>
-             © 2023 Leki Emmanuel. <i className="fab fa-heart"></i> All Rights
-             Reserved.
-          </p>
-        </div>
+      <div className="copyright">
+        <p>
+          © 2023 - {currentYear} Leki Emmanuel. <i className="fab fa-heart"></i> All Rights Reserved.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
+
